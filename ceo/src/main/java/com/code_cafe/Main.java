0e87d8395a138801e9cab26314a0365affc38214
Swapnali <swapnalipatil976@gmail.com>
2024-06-27 12:17:43 +0530

    Profile page added

--- ceo/src/main/java/com/code_cafe/Main.java
+++ ceo/src/main/java/com/code_cafe/Main.java
@@ -1,6 +1,7 @@
 package com.code_cafe;
 
 import com.code_cafe.HomePage.Home;
+import com.code_cafe.ProfilePage.Profile;
 
 import javafx.application.Application;
 
@@ -8,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Application.launch(Home.class, args);
+
     }
 }
