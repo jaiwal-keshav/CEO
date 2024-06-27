69a271ad7be520caccfc46ec10367da2da2ff288
janhavikhopade <janhavi.khopade27@gmail.com>
2024-06-27 12:50:05 +0530

    MessagePage Images Added

--- ceo/src/main/java/com/code_cafe/Main.java
+++ ceo/src/main/java/com/code_cafe/Main.java
@@ -9,8 +9,8 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Application.launch(Home.class, args);
-        Application.launch(Profile.class, args);
+        //Application.launch(Home.class, args);
+        //Application.launch(Profile.class, args);
         Application.launch(Message.class, args);
 
     }
